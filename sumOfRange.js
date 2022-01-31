a7729bcbd827d73e3171ddc1274c2b3b83ac5df9
Tzekov <stanislavtz@gmail.com>
2022-01-31 15:56:28 +0200

    Add conditional statements

--- sumOfRange.js
+++ sumOfRange.js
@@ -2,12 +2,20 @@ function range(start, end, step = 1) {
     const result = [];
     
     const setArray = (start, end, step) => {
-        if (start === end) {
-            result.push(start);
-        } else {
+        if(start > end && step < 0) {
             result.push(start);
             return setArray(start + step, end, step);
         }
+
+        if(start < end && step > 0) {
+            result.push(start);
+            return setArray(start + step, end, step);
+        }
+
+        if (start === end) {
+            result.push(start);
+        } 
+
         return result;
     }
 
@@ -18,7 +26,7 @@ const sum = (arr) => arr.reduce((acc, el) => acc + el, 0);
 
 console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(range(5, 2, -1));
+console.log(range(5, 2, -2));
 // → [5, 4, 3, 2]
 console.log(sum(range(1, 10)));
 // → 55
