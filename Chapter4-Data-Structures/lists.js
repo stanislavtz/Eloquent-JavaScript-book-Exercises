92740c23c5114aaf8f54744ff332d79862fb31cc
Tzekov <stanislavtz@gmail.com>
2022-02-01 17:25:54 +0200

    Add lists solution

---
+++ Chapter4-Data-Structures/lists.js
@@ -0,0 +1,24 @@
+const arrayToList = (array) => {
+    const result = {};
+
+    for (let i = 0; i < array.length; i++) {
+        
+    }
+
+
+    return result;
+}
+
+// Your code here.
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+
+// console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+
+// console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+
+// console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
