1583329f9c500e1f257c7bfa8017fbd655d2eb68
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-02-14 00:31:34 +0200

    Add list solution

--- Chapter4/lists.js
+++ Chapter4/lists.js
@@ -1,10 +1,23 @@
 const arrayToList = (array) => {
-   
+    let  top = {
+        value: array[array.length - 1],
+        rest: null,
+    }
+
+    for (let i = array.length - 2; i >= 0; i--) {
+        const node = {
+            value: array[i],
+            rest: top,
+        }
+
+        top = node;
+    }
+
+    return top
 }
+// Your code here.
 
-    // Your code here.
-
-    console.log(arrayToList([10, 20, 30]));
+console.log(arrayToList([10, 20, 30]));
 // → {value: 10, rest: {value: 20, rest: null}}
 
 // console.log(listToArray(arrayToList([10, 20, 30])));
