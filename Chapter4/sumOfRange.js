961c552b4cf1643d3b9dd0e3ffe913ab65834584
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-02-19 08:22:08 +0200

    Code refactoring

--- Chapter4/sumOfRange.js
+++ Chapter4/sumOfRange.js
@@ -1,32 +1,33 @@
 function range(start, end, step = 1) {
     const result = [];
+
+    function setArray(begin) {
+        if(begin > end && step < 0) {
+            result.push(begin);
+            return setArray(begin+step);
+        }
+        
+        if(begin < end && step > 0) {
+            result.push(begin);
+            return setArray(begin+step);
+        }
     
-    const setArray = (start, end, step) => {
-        if(start > end && step < 0) {
-            result.push(start);
-            return setArray(start + step, end, step);
-        }
-
-        if(start < end && step > 0) {
-            result.push(start);
-            return setArray(start + step, end, step);
-        }
-
-        if (start === end) {
-            result.push(start);
+        if (begin === end) {
+            result.push(begin);
         } 
-
+    
         return result;
     }
 
-    return setArray(start, end, step);
+    return setArray(start);
 }
 
-const sum = (arr) => arr.reduce((acc, el) => acc + el, 0);
+
+const sum = (arr) => arr.reduce((acc, el) => acc + el);
 
 console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(range(5, 2, -2));
+console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
 console.log(sum(range(1, 10)));
 // → 55
