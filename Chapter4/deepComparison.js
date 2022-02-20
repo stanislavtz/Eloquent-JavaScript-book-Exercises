3a44a7d2405c8fe4f74658c3feeed56318582cc8
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-02-21 00:35:08 +0200

    temporary upload

---
+++ Chapter4/deepComparison.js
@@ -0,0 +1,11 @@
+function deepEqual(obj1, obj2) {
+    return typeof obj1 === typeof obj2;
+}
+
+let obj = {here: {is: "an"}, object: 2};
+
+console.log(deepEqual(obj, obj));
+
+console.log(deepEqual(obj, {here: 1, object: 2}));
+
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
