8e5aeae0c1fa100b596e3a25c917b8982d1b102b
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-03-03 21:21:27 +0200

    Add regexGolf temporary solutin

---
+++ Chapter9/regexGolf.js
@@ -0,0 +1,44 @@
+// Fill in the regular expressions
+
+verify(/ca[rt]+/,
+    ["my car", "bad cats"],
+    ["camper", "high art"]);
+
+verify(/pr?op/,
+    ["pop culture", "mad props"],
+    ["plop", "prrrop"]);
+
+verify(/^ferr[^um]/,
+    ["ferret", "ferry", "ferrari"],
+    ["ferrum", "transfer A"]);
+
+verify(/(.*)(ious$|ious \w+)/,
+    ["how delicious", "spacious room"],
+    ["ruinous", "consciousness"]);
+
+// verify(/.../,
+//     ["bad punctuation ."],
+//     ["escape the period"]);
+
+// verify(/.../,
+//     ["Siebentausenddreihundertzweiundzwanzig"],
+//     ["no", "three small words"]);
+
+// verify(/.../,
+//     ["red platypus", "wobbling nest"],
+//     ["earth bed", "learning ape", "BEET"]);
+
+
+function verify(regexp, yes, no) {
+    // Ignore unfinished exercises
+    if (regexp.source == "...") return;
+    for (let str of yes) {
+        console.log(str.match(regexp))
+        if (!regexp.test(str)) {
+            console.log(`Failure to match '${str}'`);
+        }
+    }
+    for (let str of no) if (regexp.test(str)) {
+        console.log(`Unexpected match for '${str}'`);
+    }
+}
