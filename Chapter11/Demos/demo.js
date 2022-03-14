919f74bae3535e3785716dabf5d41832a89446f9
Tzekov <stanislavtz@gmail.com>
2022-03-14 17:01:59 +0200

    Crow nets updates

--- Chapter11/Demos/demo.js
+++ Chapter11/Demos/demo.js
@@ -1,3 +1,11 @@
 setTimeout(() => {
     console.log('BlaBleuBlee')
-}, 2000);
+}, 20);
+
+new Promise((_, reject) => reject(new Error("Fail")))
+    .then(value => "Handler 1")
+    .then(value => console.log("Handler 2", value))
+    .catch(reason => {
+        console.log("Caught failure " + reason.message);
+        return "nothing";
+    })
