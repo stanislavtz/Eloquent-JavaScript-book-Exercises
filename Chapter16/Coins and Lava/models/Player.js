0dcd2c4a548a05aa072e379fec1d4595554faa46
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-03-30 22:34:21 +0300

    Add Player object

---
+++ Chapter16/Coins and Lava/models/Player.js
@@ -0,0 +1,17 @@
+import Vec from "./Vector";
+
+class Player {
+    constructor(pos, speed) {
+        this.pos = pos;
+        this.speed = speed;
+    }
+
+    // type = () => 'player';
+    get type() {return 'player'}
+
+    static create(pos) {
+        let position = pos.plus(new Vec(0, -0.5));
+        let speed = new Vec(0, 0);
+        return new Player(position, speed);
+      }
+}
