ca9bbcdbc454d64cd4de7b4e561e1a065f92de64
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-03-30 22:31:02 +0300

    Add Level object

---
+++ Chapter16/Coins and Lava/models/Level.js
@@ -0,0 +1,21 @@
+class Level {
+    constructor(plan) {
+        let rows = plan.trim().split("\n").map(l => [...l]);
+        this.height = rows.length;
+        this.width = rows[0].length;
+        this.startActors = [];
+
+        this.rows = rows.map((row, y) => {
+            return row.map((ch, x) => {
+                let type = levelChars[ch];
+                if (typeof type == "string") {
+                    return type;
+                }
+                this.startActors.push(type.create(new Vec(x, y), ch));
+                return "empty";
+            });
+        });
+    }
+}
+
+export default Level;
