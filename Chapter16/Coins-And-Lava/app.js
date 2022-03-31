06961863d43d2173bbfa0f9624ba8bf83cd8392c
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-03-31 08:43:47 +0300

    Perform tests for getting types

--- Chapter16/Coins-And-Lava/app.js
+++ Chapter16/Coins-And-Lava/app.js
@@ -1,4 +1,14 @@
 import simpleLevelPlan from './utils/levelPlan.js';
 import Level from './models/Level.js';
-
+import Actor from './models/actors/Actor.js';
+import Player from './models/actors/Player.js';
+import Lava from './models/actors/Lava.js';
 // const level = new Level(simpleLevelPlan);
+
+const actor = new Actor(5, 6);
+const player = new Player(5, 6);
+const lava = new Lava(5, 6, -1);
+
+console.log(actor.type);
+console.log(player.type);
+console.log(lava.type);
