e237ad8a85ffc23d15188d0065f4e29fe1a24376
Tzekov <stanislavtz@gmail.com>
2022-04-01 14:54:16 +0300

    Display simpleLevel

--- Chapter16/Coins-And-Lava/app.js
+++ Chapter16/Coins-And-Lava/app.js
@@ -1,18 +1,13 @@
-import simpleLevelPlan from './utils/levelPlan.js';
 import Level from './models/Level.js';
+import State from './models/State.js';
+import DOMDisplay from './models/DomDisplay.js';
+import simpleLevelPlan from './utils/levelPlan.js';
 import Actor from './models/actors/Actor.js';
 import Player from './models/actors/Player.js';
 import Lava from './models/actors/Lava.js';
-const level = new Level(simpleLevelPlan);
-let planInfo = `Simple level dimensions: ${level.width} x ${level.height}`;
 
-const div = document.createElement('div');
-div.textContent = planInfo;
-document.body.appendChild(div)
-// const actor = new Actor(5, 6);
-// const player = new Player(5, 6);
-// const lava = new Lava(5, 6, -1);
-
-// console.log(actor.type);
-// console.log(player.type);
-// console.log(lava.type);
+let simpleLevel = new Level(simpleLevelPlan);
+console.log(simpleLevel.startActors);
+console.log(simpleLevel.rows);
+let display = new DOMDisplay(document.body, simpleLevel);
+display.syncState(State.start(simpleLevel));
