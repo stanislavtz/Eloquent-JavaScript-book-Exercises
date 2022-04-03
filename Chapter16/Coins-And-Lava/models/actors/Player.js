1ba9fe2816aa505a6be60ed3cfb68880819d0e3f
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-04-03 23:44:51 +0300

    Add update method and statick lives

--- Chapter16/Coins-And-Lava/models/actors/Player.js
+++ Chapter16/Coins-And-Lava/models/actors/Player.js
@@ -1,7 +1,8 @@
 import Actor from "./Actor.js";
 import Vec from "../Vector.js";
+import { PLAYER_X_SPEED, JUMP_SPEED, GRAVITY } from '../../constants/player.js';
 
-class Player extends Actor{
+class Player extends Actor {
     constructor(pos, speed) {
         super(pos, speed);
     }
@@ -10,9 +11,35 @@ class Player extends Actor{
         let position = pos.plus(new Vec(0, -0.5));
         let speed = new Vec(0, 0);
         return new Player(position, speed);
-      }
+    }
+
+    static LIVES = 3;
 }
 
 Player.prototype.size = new Vec(0.8, 1.5);
+
+Player.prototype.update = function (time, state, keys) {
+    let xSpeed = 0;
+    if (keys.ArrowLeft) { xSpeed -= PLAYER_X_SPEED; }
+    if (keys.ArrowRight) { xSpeed += PLAYER_X_SPEED; }
+
+    let pos = this.pos;
+    let movedX = pos.plus(new Vec(xSpeed * time, 0));
+    if (!state.level.touches(movedX, this.size, "wall")) {
+        pos = movedX;
+    }
+
+    let ySpeed = this.speed.y + time * GRAVITY;
+    let movedY = pos.plus(new Vec(0, ySpeed * time));
+    if (!state.level.touches(movedY, this.size, "wall")) {
+        pos = movedY;
+    } else if (keys.ArrowUp && ySpeed > 0) {
+        ySpeed = -JUMP_SPEED;
+    } else {
+        ySpeed = 0;
+    }
+
+    return new Player(pos, new Vec(xSpeed, ySpeed));
+};
 
 export default Player;
