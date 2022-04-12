e126165dc4d163c377bde633bd8d34796cd3830d
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-04-12 07:47:06 +0300

    Update demo.js

--- Chapter20/demo.js
+++ Chapter20/demo.js
@@ -1,9 +1,31 @@
-const reverse = require('./showArgs.js');
-const { readFile, writeFile, rm } = require('fs').promises;
+const { createServer } = require('http');
 
-// let demo = reverse(process.argv[2]);
-// console.log(demo);
+const port = 8080;
+const greeting = 'Hello world!';
+const pTag = 'I am a paragraph tag';
+const hrefURL = `https://google.bg`;
 
-rm('../../demoText.txt')
-    .then(() => console.log('The file was removed!'))
-    .catch(err => console.error(err));
+const body = (`
+        <h1>${greeting}</h1>
+        <p>${pTag}</p>
+        <a href=${hrefURL}>click me</a>
+    `);
+
+const server1 = createServer((req, res) => {
+    res.writeHead(200, {'content-type' : 'text/html'});
+    res.write(body, 'utf-8');
+    res.end();
+});
+
+let server = createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    // console.log(request)
+    response.write(`
+    <h1>Hello!</h1>
+    <p>You asked for <code>${request.method}</code></p>`);
+    response.end();
+});
+server.listen(8000);
+console.log("Listening! (port 8000)");
+
+server1.listen(port, () => console.log(`Server is running on port ${port}...`));
