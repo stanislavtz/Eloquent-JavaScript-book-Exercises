44cc6a28ba937738973f2d832a010764403c3f30
Tzekov <stanislavtz@gmail.com>
2022-04-13 15:13:04 +0300

    Add path join instead decodeing URL and resolve path

--- Chapter20/demo.js
+++ Chapter20/demo.js
@@ -5,14 +5,14 @@ const methods = Object.create(null);
 const app = createServer((request, response) => {
     let handler = methods[request.method] || notAllowed;
     handler(request)
-        .catch(error => {
-            if (error.status != null) return error;
-            return { body: String(error), status: 500 };
-        })
         .then(({ body, status = 200, type = "text/plain" }) => {
             response.writeHead(status, { "Content-Type": type });
             if (body && body.pipe) body.pipe(response);
             else response.end(body);
+        })
+        .catch(error => {
+            if (error.status != null) return error;
+            return { body: String(error), status: 500 };
         });
 })
 
@@ -26,13 +26,16 @@ async function notAllowed(request) {
 }
 
 var { parse } = require("url");
-var { resolve, sep } = require("path");
+
+var { join, sep } = require("path");
 
 var baseDirectory = process.cwd();
 
 function urlPath(url) {
     let { pathname } = parse(url);
-    let path = resolve(decodeURIComponent(pathname).slice(1));
+    let path = join(__dirname, pathname);
+    // let path = resolve(decodeURIComponent(pathname).slice(1));
+
     if (path != baseDirectory &&
         !path.startsWith(baseDirectory + sep)) {
         throw { status: 403, body: "Forbidden" };
@@ -80,6 +83,7 @@ methods.DELETE = async function (request) {
 };
 
 const { createWriteStream } = require("fs");
+const path = require("path");
 
 function pipeStream(from, to) {
     return new Promise((resolve, reject) => {
